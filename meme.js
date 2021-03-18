163995974633097fa893cf9a62ada341aeaf89cd
Celine <80287095+yceline-yu@users.noreply.github.com>
2021-03-17 21:28:55 -0700

    Update meme.js

--- meme.js
+++ meme.js
@@ -1,9 +1,9 @@
-let subBut = document.querySelector("form");
-subBut.addEventListener("submit", function(event){
+
+document.addEventListener("submit", function(event){
     event.preventDefault();
     let inputs = document.getElementById("initial-form").elements;
     let top = inputs[1].value;
     let bot = inputs[2].value;
-  alert(top);
+    let img = inputs[0].value;
 })
 
