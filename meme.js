342fd41608cb3ec85a64f0a95b5603835aea082f
Celine <80287095+yceline-yu@users.noreply.github.com>
2021-03-19 20:15:49 -0700

    Update meme.js

--- meme.js
+++ meme.js
@@ -1,6 +1,10 @@
-
 document.addEventListener("submit", function(event){
     event.preventDefault();
+    meme-content.appendChild(meme);
+})
+
+
+    function memes(imgs, top, bot){
     let inputs = document.getElementById("initial-form").elements;
     let top = inputs[1].value;
     let bot = inputs[2].value;
@@ -19,7 +23,7 @@ document.addEventListener("submit", function(event){
     meme.append(img);
     meme.append(botTx);
     
-    
-    return meme-content.appendChild(meme);
-})
+    return meme
+    }
+
 
